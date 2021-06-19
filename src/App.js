26df86eacd65451e16fc929a49396b09cd9e1be6
Hector <dev@hsmith.org>
2021-06-19 14:37:36 +0000

    feat: create basic ui wireframe
    Re-write the page HTML to create a basic page wireframe. This wireframe
    does not contain any logic and instead only focuses on rendering the right
    components in the right place.
    Add the ForkAwesome repo to the project to handle rendering icons.
    Support submodules in gitlab CI/CD.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,145 @@
-function App() {
-  return (
-    <div className="container">
-      <h1 className="title">Title</h1>
-      <h2 className="subtitle">Sub-Title</h2>
-      <div className="has-text-centered">
-        <p>Some more content goes here</p>
-      </div>
-    </div>
-  );
+import {Component} from "react";
+
+export default class App extends Component {
+    constructor(props) {
+        super(props)
+        this.state = { selectedGhost: "" }
+    }
+    render() {
+        return (
+            <div className="container">
+                <TopNav />
+                { this.state.selectedGhost !== "" ? <GhostInfoModal ghost={this.state.selectedGhost} /> : "" }
+
+                <div className="columns">
+                    <div className="column is-4">
+                        <LeftColumn />
+                    </div>
+                    <div className="column is-8">
+                        <RightColumn />
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
 
-export default App;
+class GhostInfoModal extends Component {
+    render() {
+        const evidence = ["Fingerprints", "Spirit Box", "Ghost Writing"]
+        return (
+            <div className="modal is-active">
+                <div className="modal-background" />
+                <div className="modal-card">
+                    <header className="modal-card-head">
+                        <p className="modal-card-title">{this.props.ghost}</p>
+                        <button className="delete" aria-label="close" />
+                    </header>
+                    <section className="modal-card-body">
+                        <p>Some content about the spirit</p>
+
+                        <div className="my-5 columns">
+                            {evidence.map((e) => {
+                                return (
+                                    <div key={e} className="column has-text-centered">
+                                        <p>{e}</p>
+                                        { e.length > 10 ? "X" : "" }
+                                    </div>
+                                )
+                            })}
+                        </div>
+                    </section>
+                </div>
+            </div>
+        )
+    }
+}
+
+class LeftColumn extends Component {
+    render() {
+        return (
+            <div className="has-text-centered">
+                <h2 className="subtitle">Observations</h2>
+
+                <ObservationToggle name="Fingerprints" />
+            </div>
+        )
+    }
+}
+
+class RightColumn extends Component {
+    render() {
+        return (
+            <div className="has-text-centered">
+                <h2 className="subtitle">Guide</h2>
+
+                <table className="table is-striped is-fullwidth">
+                    <thead>
+                        <GhostTableHeader />
+                    </thead>
+                    <tbody>
+                        <GhostTableRow name="Spirit" evidence={["Fingerprints", "Spirit Box", "Ghost Writing"]} />
+                        <GhostTableRow name="Wraith" evidence={["Fingerprints", "Spirit Box", "Freezing"]} />
+                    </tbody>
+                </table>
+            </div>
+        )
+    }
+}
+
+class GhostTableHeader extends Component {
+    render() {
+        return (
+            <tr>
+                <th>Name</th>
+                <th/>
+                <th/>
+                <th/>
+            </tr>
+        )
+    }
+}
+
+class GhostTableRow extends Component {
+    render() {
+        return (
+            <tr>
+                <td>
+                    {this.props.name}
+                    <a className="icon has-text-info pull-right" onClick={console.log} href={`#${this.props.name}`}>
+                        <i className="fa fa-info-circle" />
+                    </a>
+                </td>
+                {this.props.evidence.map((evidence) => {
+                    return (
+                        <td key={evidence}>
+                            {evidence}
+                        </td>)
+                })}
+            </tr>
+        )
+    }
+}
+
+class ObservationToggle extends Component {
+    render() {
+        return (
+            <div className="has-text-centered">
+                <label className="checkbox">
+                    <input type="checkbox" />
+                    <p>{this.props.name}</p>
+                </label>
+            </div>
+        );
+    }
+}
+
+class TopNav extends Component {
+    render() {
+        return (
+            <div className="my-6 has-text-centered">
+                <h1 className="title">Paranormal Guide</h1>
+            </div>
+        )
+    }
+}

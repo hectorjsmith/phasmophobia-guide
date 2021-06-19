6cc7edd7063335b7fdaf33e5afcf8d0752e1c54d
Hector <dev@hsmith.org>
2021-06-19 07:20:12 +0100

    refactor: remove boilerplate code and add theme
    Remove all the unnecessary boilerplate code from the react starter app.
    Add a theme to the website using Bulma and Bulmaswatch.
    Update project name in various files.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="title">Title</h1>
+      <h2 className="subtitle">Sub-Title</h2>
+      <div className="has-text-centered">
+        <p>Some more content goes here</p>
+      </div>
     </div>
   );
 }

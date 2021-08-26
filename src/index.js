2c311f6b9910c9c3b659852d453e779d6792098e
Hector <dev@hsmith.org>
2021-08-26 21:15:44 +0000

    feat: support phasmophobia v0.3.0 (#7)
    Update the app data to include the new ghosts with metadata and links to
    their corresponding wiki pages.
    Add the new "D.O.T.S. Projector" evidence type.
    Update evidence for all existing ghosts based on the game wiki page.

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ const ghostWriting = "Ghost Writing"
 const freezing = "Freezing"
 const emf = "EMF"
 const ghostOrb = "Ghost Orb"
+const dotsProjector = "D.O.T.S. Projector"
 
 const collectEvidence = function() {
     return [
@@ -23,6 +24,7 @@ const collectEvidence = function() {
         new Evidence(spiritBox, false, false),
         new Evidence(ghostWriting, false, false),
         new Evidence(fingerprints, false, false),
+        new Evidence(dotsProjector, false, false)
     ]
 }
 
@@ -30,7 +32,7 @@ const collectGhostData = function() {
     return [
         new Ghost(
             "Phantom",
-            [freezing, emf, ghostOrb],
+            [dotsProjector, fingerprints, spiritBox],
             "https://phasmophobia.fandom.com/wiki/Phantom",
             "A Phantom is a Ghost that can possess the living, most commonly summoned by a Ouija Board. It also induces fear into those around it.",
             ["Taking a photo of the Phantom will make it temporarily disappear"],
@@ -38,7 +40,7 @@ const collectGhostData = function() {
         ),
         new Ghost(
             "Banshee",
-            [freezing, emf, fingerprints],
+            [dotsProjector, fingerprints, ghostOrb],
             "https://phasmophobia.fandom.com/wiki/Banshee",
             "A Banshee is a natural hunter and will attack anything. It has been known to stalk its prey one at a time until making its kill.",
             ["Banshees fear the Crucifix"],
@@ -46,7 +48,7 @@ const collectGhostData = function() {
         ),
         new Ghost(
             "Mare",
-            [freezing, ghostOrb, spiritBox],
+            [ghostOrb, ghostWriting, spiritBox],
             "https://phasmophobia.fandom.com/wiki/Mare",
             "A Mare is the source of all nightmares, making it most powerful in the dark.",
             ["Turning the lights on will lower its chance to attack"],
@@ -54,7 +56,7 @@ const collectGhostData = function() {
         ),
         new Ghost(
             "Yurei",
-            [freezing, ghostOrb, ghostWriting],
+            [dotsProjector, freezing, ghostOrb],
             "https://phasmophobia.fandom.com/wiki/Yurei",
             "A Yurei is a Ghost that has returned to the physical world, usually for the purpose of revenge or hatred.",
             ["Using Smudge Sticks on the Yurei will cause it to not wander"],
@@ -62,7 +64,7 @@ const collectGhostData = function() {
         ),
         new Ghost(
             "Demon",
-            [freezing, spiritBox, ghostWriting],
+            [fingerprints, freezing, ghostWriting],
             "https://phasmophobia.fandom.com/wiki/Demon",
             "A Demon is one of the worst Ghosts you can encounter. It has been known to attack without a reason.",
             ["Asking a Demon successful questions on the Ouija Board won't lower the user's sanity"],
@@ -70,7 +72,7 @@ const collectGhostData = function() {
         ),
         new Ghost(
             "Wraith",
-            [freezing, spiritBox, fingerprints],
+            [dotsProjector, emf, spiritBox],
             "https://phasmophobia.fandom.com/wiki/Wraith",
             "A Wraith is one of the most dangerous Ghosts you will find. It is also the only known ghost that has the ability of flight and has sometimes been known to travel through walls.",
             ["Wraiths have a toxic reaction to Salt"],
@@ -78,7 +80,7 @@ const collectGhostData = function() {
         ),
         new Ghost(
             "Jinn",
-            [emf, ghostOrb, spiritBox],
+            [emf, fingerprints, freezing],
             "https://phasmophobia.fandom.com/wiki/Jinn",
             "A Jinn is a territorial Ghost that will attack when threatened. It also has been known to travel at significant speed.",
             ["Turning off the location's power source will prevent the Jinn from using its ability"],
@@ -86,7 +88,7 @@ const collectGhostData = function() {
         ),
         new Ghost(
             "Shade",
-            [emf, ghostOrb, ghostWriting],
+            [emf, freezing, ghostWriting],
             "https://phasmophobia.fandom.com/wiki/Shade",
             "A Shade is known to be a Shy Ghost. There is evidence that a Shade will stop all paranormal activity if there are multiple people nearby.",
             ["A Shade will rarely start a Hunt when players are grouped together"],
@@ -94,7 +96,7 @@ const collectGhostData = function() {
         ),
         new Ghost(
             "Oni",
-            [emf, spiritBox, ghostWriting],
+            [dotsProjector, emf, freezing],
             "https://phasmophobia.fandom.com/wiki/Oni",
             "Onis are a cousin to the Demon and possess extreme strength. There have been rumors that they become more active around their prey.",
             ["More active when people nearby", "Throws objects at great speed"],
@@ -102,7 +104,7 @@ const collectGhostData = function() {
         ),
         new Ghost(
             "Revenant",
-            [emf, ghostWriting, fingerprints],
+            [freezing, ghostOrb, ghostWriting],
             "https://phasmophobia.fandom.com/wiki/Revenant",
             "A Revenant is a slow but violent Ghost that will attack indiscriminantly. It has been rumored to travel at a significantly high speed when hunting.",
             ["Hiding from the Revenant will cause it to move at a significantly reduced speed"],
@@ -110,7 +112,7 @@ const collectGhostData = function() {
         ),
         new Ghost(
             "Poltergeist",
-            [ghostOrb, spiritBox, fingerprints],
+            [fingerprints, ghostWriting, spiritBox],
             "https://phasmophobia.fandom.com/wiki/Poltergeist",
             "One of the most famous Ghosts, a Poltergeist, also known as a noisy ghost can manipulate objects around it to spread fear into it's victims.",
             ["Innefective in empty rooms"],
@@ -118,7 +120,7 @@ const collectGhostData = function() {
         ),
         new Ghost(
             "Spirit",
-            [spiritBox, ghostWriting, fingerprints],
+            [emf, ghostWriting, spiritBox],
             "https://phasmophobia.fandom.com/wiki/Spirit",
             "A Spirit is the most common Ghost you will come across however it is still very powerful and dangerous. They are usually discovered at one of their hunting grounds after an unexplained death.",
             ["Smudge sticks"],
@@ -126,7 +128,7 @@ const collectGhostData = function() {
         ),
         new Ghost(
             "Hantu",
-            [ghostOrb, ghostWriting, fingerprints],
+            [fingerprints, freezing, ghostOrb],
             "https://phasmophobia.fandom.com/wiki/Hantu",
             "A rare ghost that can be found in hot climates. They are known to attack more often in cold weather.",
             ["Hantu moves slower in warmer areas"],
@@ -134,12 +136,28 @@ const collectGhostData = function() {
         ),
         new Ghost(
             "Yokai",
-            [ghostOrb, spiritBox, ghostWriting],
+            [dotsProjector, ghostOrb, spiritBox],
             "https://phasmophobia.fandom.com/wiki/Yokai",
             "A common type of ghost that is attracted to human voices. They can usually be found haunting family homes.",
             ["While hunting, it can only hear voices close to it"],
             ["Talking near a Yokai will anger it and cause it to attack more often"]
         ),
+        new Ghost(
+            "Goryo",
+            [dotsProjector, emf, fingerprints],
+            "https://phasmophobia.fandom.com/wiki/Goryo",
+            "Using a video camera is the only way to view a Goryo, when it passes through a DOTS projector",
+            ["A Goryo will usually only show itself on camera if there are no people nearby"],
+            ["They are rarely seen far from their place of death"]
+        ),
+        new Ghost(
+            "Myling",
+            [emf, fingerprints, ghostWriting],
+            "https://phasmophobia.fandom.com/wiki/Myling",
+            "A Myling is a very vocal and active ghost. They are rumoured to be quiet when hunting their prey.",
+            ["A Myling is known to be quieter when hunting"],
+            ["Mylings more frequently make paranormal sounds"]
+        )
     ]
 }
 

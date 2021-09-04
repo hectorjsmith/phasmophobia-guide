f285065003d7d164820d51a45a69ea8c883cd2d8
Hector <dev@hsmith.org>
2021-09-04 10:50:55 +0100

    update sync button location in header
    Update the header layout to render the sync button in the correct location
    on both desktop and mobile (to the right on desktop, centered on mobile).
    Add new style to push the sync button up slightly.

--- src/nav/Header.js
+++ src/nav/Header.js
@@ -21,17 +21,20 @@ export default class TopNav extends Component {
 
     render() {
         return (
-            <nav className="level">
-                <p className="level-item has-text-centered">
+            <nav className="columns is-multiline is-vcentered mb-5">
+                <div className="column is-2-desktop" />
+                <p className="column is-8-desktop is-12-mobile has-text-centered">
                     <img src={headerImg} alt="Phasmophobia paranormal guide" style={{height: "150px"}} />
                 </p>
-                <button className={"button is-outlined" + (this.state.syncing ? " is-success" : "")}
-                        onClick={this.props.openSyncModal}>
-                    <span className="icon mr-3">
-                        <i className={"fa fa-sync" + (this.state.syncing ? " fa-spin" : "")} />
-                    </span>
-                    {this.state.syncing ? "Connected" : "Sync"}
-                </button>
+                <div className="column is-2-desktop is-12-mobile has-text-centered mt--5">
+                    <button className={"button is-outlined" + (this.state.syncing ? " is-success" : "")}
+                            onClick={this.props.openSyncModal}>
+                        <span className="icon mr-3">
+                            <i className={"fa fa-sync" + (this.state.syncing ? " fa-spin" : "")} />
+                        </span>
+                        {this.state.syncing ? "Connected" : "Sync"}
+                    </button>
+                </div>
             </nav>
         )
     }

adbb62a84054894838cef6a03be18fb1ad31ea0e
Hector <dev@hsmith.org>
2021-11-23 22:03:19 +0000

    very rough draft of conversion to functional components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,68 @@
-import {Component} from "react"
+import {Component, useEffect} from "react"
 import Footer from "./nav/Footer"
 import TopNav from "./nav/Header"
 import LeftColumn from "./layout/LeftColumn"
 import RightColumn from "./layout/RightColumn"
 
-export default class App extends Component {
+export default App = ({evidence, ghosts}) => {
+    const [evidenceData, setEvidenceData] = useState([])
+    const [possibleGhosts, setPossibleGhosts] = useState([])
+
+    const mapEvidence = (e) => {
+        return e.map((e1) => {
+            return {
+                ...e1,
+                selected: false,
+                rejected: false
+            }
+        })
+    }
+
+    const filterGhosts = (evidence) => {
+        let selectedEvidence = evidence.filter(e => e.selected)
+        let rejectedEvidence = evidence.filter(e => e.rejected)
+
+        if (selectedEvidence.length === 0 && rejectedEvidence.length === 0) {
+            return ghosts
+        }
+
+        return ghosts.filter(ghost => {
+            let ghostHasSelectedEvidence = selectedEvidence.length === 0
+                || selectedEvidence.every(selected => ghost.evidence.some(ghostEvidence => ghostEvidence === selected.name))
+            let ghostHasRejectedEvidence = rejectedEvidence.length > 0
+                && rejectedEvidence.some(rejected => ghost.evidence.some(ghostEvidence => ghostEvidence === rejected.name))
+
+            return ghostHasSelectedEvidence && !ghostHasRejectedEvidence
+        })
+    }
+
+    useEffect(() => setEvidenceData(mapEvidence(evidence)), [evidence])
+    useEffect(() => setPossibleGhosts(filterGhosts(evidence)), [evidence, ghosts])
+
+    return (
+        <div className="content-wrapper">
+            <div className="content-main">
+                <div className="container">
+                    <TopNav />
+                    <div className="columns">
+                        <div className="column is-4">
+                            <LeftColumn evidence={evidenceData}
+                                        setEvidence={setEvidenceData}
+                                        possibleGhosts={possibleGhosts} />
+                        </div>
+                        <div className="column is-8">
+                            <RightColumn evidence={this.state.evidence}
+                                         ghosts={this.state.possibleGhosts}/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <Footer />
+        </div>
+    )
+}
+
+export class App extends Component {
     constructor(props) {
         super(props)
         this.state = {
--- src/layout/LeftColumn.js
+++ src/layout/LeftColumn.js
@@ -1,6 +1,52 @@
 import {Component} from "react"
 import { compareStringsAsc } from "../util/stringSort"
-import ObservationToggle from "./ObservationToggle"
+import { ObservationToggle } from "./ObservationToggle"
+
+const LeftColumn = ({evidence, setEvidence, possibleGhosts}) => {
+    const maxSelected = 3
+    const countSelectedEvidence = () => {
+        return evidence.filter(e => e.selected).length
+    }
+    const countRejectedEvidence = () => {
+        return evidence.filter(e => e.rejected).length
+    }
+
+    const showWarning = () => {
+        if (countSelectedEvidence() > maxSelected) {
+            return (
+                <span className="icon has-text-warning" title="Too many observations selected">
+                    <i className="fa fa-2x fa-warning" />
+                </span>
+            )
+        }
+        return ""
+    }
+
+    let selectedEvidenceCount = countSelectedEvidence()
+    let rejectedEvidenceCount = countRejectedEvidence()
+    let selectionCount = selectedEvidenceCount + rejectedEvidenceCount
+    return (
+        <div>
+            <h2 className="mb-5 has-text-centered is-size-5 is-uppercase has-letter-spacing">Observations</h2>
+
+            {evidence.sort((a, b) => compareStringsAsc(a.name, b.name)).map((e) => {
+                return <ObservationToggle key={e.name}
+                                          evidence={e}
+                                          setEvidence={e}
+                                          possibleGhosts={possibleGhosts} />
+            })}
+
+            <div className="has-text-centered">
+                <button className={"button my-4 is-outlined" + (selectionCount > 0 ? " is-warning" : "")}
+                        onClick={(e) => {this.props.onEvidenceReset(); console.log(e.target)}}
+                        title="Reset all selected observations">reset</button>
+
+                <p className="heading mb-3">({selectedEvidenceCount} of {this.maxSelected})</p>
+                {this.showWarning()}
+            </div>
+        </div>
+    )
+}
 
 export default class LeftColumn extends Component {
     maxSelected = 3
@@ -34,6 +80,7 @@ export default class LeftColumn extends Component {
                 {this.props.evidence.sort((a, b) => compareStringsAsc(a.name, b.name)).map((evidence) => {
                     return <ObservationToggle key={evidence.name}
                                               evidence={evidence}
+                                              setEvidence={}
                                               possibleGhosts={this.props.possibleGhosts}
                                               onToggle={this.props.onEvidenceToggle} />
                 })}

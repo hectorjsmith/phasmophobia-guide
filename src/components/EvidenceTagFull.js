1323019d45259d7937d969a8af8ac11efc757e47
Hector <hector@hjs.dev>
2024-06-08 12:54:57 +0100

    use long name on hover

--- src/components/EvidenceTagFull.js
+++ src/components/EvidenceTagFull.js
@@ -1,11 +1,11 @@
-export const EvidenceTagFull = ({ name, icon, isSelected }) => {
+export const EvidenceTagFull = ({ name, longName, icon, isSelected }) => {
   return (
     <span
       className={
         'tag is-medium' +
         (isSelected ? ' is-success' : ' is-transparent has-border')
       }
-      title={name}
+      title={longName}
     >
       <span className="icon">
         <i className={'fa fa-' + icon}></i>
